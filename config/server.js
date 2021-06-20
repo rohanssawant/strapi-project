c4c186471f6edd1f730212153e73ee18c4fa51e8
rohanssawant <rohansawant1801@gmail.com>
2021-06-20 11:14:08 +0530

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '76412e9d9bcf929a1cc895e47d038d14'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6ee9b95d-61d8-4989-8e1a-2973a04aadaa'
+};
